266f5c3333a003089b9098fd8eb481a41cd74ace
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-04-20 20:59:31 +0200

    reading source program

--- src/SourceFile.java
+++ src/SourceFile.java
@@ -1,3 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class SourceFile {
-    //merged
+
+    public static ArrayList<String>readSourceProgramFromFile(){
+        ArrayList<String> sourceProgramArray = new ArrayList<>();
+
+        BufferedReader bufferedReader;
+        try{
+            bufferedReader = new BufferedReader(new FileReader("dictionary"));
+            String line = bufferedReader.readLine();
+            while (line != null){
+                sourceProgramArray.add(line);
+                line = bufferedReader.readLine();
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return sourceProgramArray;
+    }
+
 }
