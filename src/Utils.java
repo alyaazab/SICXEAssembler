2be753a31309d752025cb74dcc0b90bc86edd923
alyaazab <alya.113@live.com>
2019-04-20 21:22:48 +0200

    Reading of fields in a line completed. Starting validations.

--- src/Utils.java
+++ src/Utils.java
@@ -3,10 +3,17 @@ public class Utils {
 
     public static boolean validateFixedFormat(String line) {
 
-        line = "abcdefghijkl";
+        line = "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890";
         String label = line.substring(0, 8);
-        System.out.println(label);
 
+        String operation = line.substring(9, 15);
+        String operand = line.substring(17, 35);
+        String comment = line.substring(35, 66);
+
+        System.out.println(label);
+        System.out.println(operation);
+        System.out.println(operand);
+        System.out.println(comment);
         return true;
 
 
