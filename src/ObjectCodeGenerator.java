94a03f0cf37180c5fecd5b57221e54eea5a9f737
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-05-13 01:56:30 +0200

    object code generation for byte directive

--- src/ObjectCodeGenerator.java
+++ src/ObjectCodeGenerator.java
@@ -3,23 +3,48 @@ import java.util.ArrayList;
 public class ObjectCodeGenerator {
 
     ArrayList<Line> lineArrayList;
-    private int n=0, i=0, x=0, b=0, p=0, e=0;
+    private int n = 0, i = 0, x = 0, b = 0, p = 0, e = 0;
     private String opcode = "";
-    private int operationFormat=0;
-    private String instructionCode = "", r1="", r2="", flags="",  disp="", address="";
+    private int operationFormat = 0;
+    private String instructionCode = "", r1 = "", r2 = "", flags = "", disp = "", address = "";
     private int baseRegisterOperand = -1;
 
     public ObjectCodeGenerator(ArrayList<Line> lineArrayList) {
         this.lineArrayList = lineArrayList;
     }
 
+    private static String leftPad(String str, int n) {
+
+        String padString;
+
+        if (n == 8) {
+            padString = "00000000";
+            if (str.length() < 8)
+                return padString.substring(str.length()) + str;
+        } else if (n == 4) {
+            padString = "0000";
+
+            if (str.length() < 4)
+                return padString.substring(str.length()) + str;
+        } else if (n == 12) {
+
+            padString = "000000000000";
+
+            if (str.length() < 12)
+                return padString.substring(str.length()) + str;
+
+        }
+
+
+        return str;
+    }
+
     public void generateObjectCode() {
-        for(Line line : lineArrayList)
-        {
+        for (Line line : lineArrayList) {
             String binaryAddress = "";
             instructionCode = "";
             //if line is a comment, skip
-            if(!line.getComment().equals(""))
+            if (!line.getComment().equals(""))
                 continue;
 
             Operation operation = line.getOperation();
@@ -27,44 +52,49 @@ public class ObjectCodeGenerator {
 
 
             //print operation
-            if(operation!=null)
-            {
+            if (operation != null) {
                 System.out.println(operation.toString());
                 operationFormat = operation.getFormat();
-            }
-            else
-            {
+            } else {
                 System.out.println("operation is null");
                 continue;
             }
 
 
-            if(operation.getOperationMnemonic().equals("base") && line.isBaseRegisterSet())
-            {
+            if (operation.getOperationMnemonic().equals("base") && line.isBaseRegisterSet()) {
                 String operand = line.getOperandField().trim();
-                if(operand.charAt(0) == '#' || operand.charAt(0) == '@')
-                {
+                if (operand.charAt(0) == '#' || operand.charAt(0) == '@') {
                     operand = operand.substring(1);
                 }
-                if(operand.charAt(operand.length()-2) == ',')
-                {
-                    operand = operand.substring(0, operand.length()-1);
+                if (operand.charAt(operand.length() - 2) == ',') {
+                    operand = operand.substring(0, operand.length() - 1);
                 }
-                if(Character.isDigit(operand.charAt(0)))
+                if (Character.isDigit(operand.charAt(0)))
                     baseRegisterOperand = Integer.valueOf(operand);
                 else
                     baseRegisterOperand = SymbolTable.getInstance().getSymbol(operand).getValue();
             }
 
 
-
-            switch(operationFormat)
-            {
+            switch (operationFormat) {
                 case -1:
                     //directive, no opcode
                     opcode = "";
                     n = i = x = b = p = e = -1;
                     System.out.println("KITTY B = " + b + "   P = " + p);
+
+                    if (operation.getOperationMnemonic().equals("byte")) {
+                        for (int i = 2; i < operandField.trim().length() - 1; i++) {
+                            if (operandField.charAt(0) == 'c') {
+                                int asciiValue = operandField.charAt(i);
+                                opcode = opcode + convertDecToHex(asciiValue);
+                                System.out.println("opcode: " + opcode);
+                            } else {
+                                opcode = opcode + operandField.charAt(i);
+                            }
+                        }
+                        n = -2;
+                    }
 
 
                     break;
@@ -75,15 +105,15 @@ public class ObjectCodeGenerator {
                     //get r1 and r2
                     r1 = leftPad(convertDecToBin(RegisterTable.getInstance().getRegTable()
                             .get(operandField.substring(0, 1)).getAddress()), 4);
-                    if(operandField.trim().length() > 1)
+                    if (operandField.trim().length() > 1)
                         r2 = leftPad(convertDecToBin(RegisterTable.getInstance().getRegTable()
-                                .get(operandField.substring(2, 3)).getAddress()),4);
+                                .get(operandField.substring(2, 3)).getAddress()), 4);
 
                     break;
 
 
                 case 3:
-                    opcode = convertHexToBin(operation.getBinaryCode()).substring(0,6);
+                    opcode = convertHexToBin(operation.getBinaryCode()).substring(0, 6);
                     System.out.println("opcode = " + opcode);
                     e = 0;
 
@@ -93,14 +123,14 @@ public class ObjectCodeGenerator {
                     int flag = 0;
 
 
-                    if(n == 1 && i == 1 && x == 0) // direct, without indexing
+                    if (n == 1 && i == 1 && x == 0) // direct, without indexing
                     {
-                       subOperand = operandField.trim();
-                    }else if (n == 1 && i == 1 && x == 1){ // direct, with indexing
+                        subOperand = operandField.trim();
+                    } else if (n == 1 && i == 1 && x == 1) { // direct, with indexing
                         subOperand = operandField.trim().substring(0, operandField.trim().length() - 2);
-                    }else if (n == 1 && i == 0){ // indirect
+                    } else if (n == 1 && i == 0) { // indirect
                         subOperand = operandField.trim().substring(1);
-                    }else if(n == 0 && i == 1){
+                    } else if (n == 0 && i == 1) {
                         subOperand = operandField.trim().substring(1);
                         try {
                             address = Integer.parseInt(subOperand);
@@ -109,7 +139,7 @@ public class ObjectCodeGenerator {
                             binaryAddress = leftPad(convertDecToBin(address), 12);
                             System.out.println("binary value: " + binaryAddress);
                             flag = 1;
-                        }catch (NumberFormatException e){
+                        } catch (NumberFormatException e) {
                             flag = 0;
                         }
                     }
@@ -121,30 +151,30 @@ public class ObjectCodeGenerator {
 
 
                 case 4:
-                    opcode = convertHexToBin(operation.getBinaryCode()).substring(0,6);
+                    opcode = convertHexToBin(operation.getBinaryCode()).substring(0, 6);
                     System.out.println("opcode = " + opcode);
                     e = 1;
                     b = 0;
                     p = 0;
                     address = -1;
                     setNIXFlags(operandField);
-                    if (n == 1 && i == 1 && x == 0){
+                    if (n == 1 && i == 1 && x == 0) {
                         address = SymbolTable.getInstance().getSymbol(operandField.trim()).getValue();
-                    }else if (n == 1 && i == 1 && x == 1){ // direct, with indexing
+                    } else if (n == 1 && i == 1 && x == 1) { // direct, with indexing
                         subOperand = operandField.trim().substring(0, operandField.trim().length() - 2);
                         address = SymbolTable.getInstance().getSymbol(subOperand).getValue();
-                    }else if (n == 1 && i == 0){ // indirect
+                    } else if (n == 1 && i == 0) { // indirect
                         subOperand = operandField.trim().substring(1);
                         address = SymbolTable.getInstance().getSymbol(subOperand).getValue();
-                    }else if(n == 0 && i == 1){
+                    } else if (n == 0 && i == 1) {
                         subOperand = operandField.trim().substring(1);
                         try {
                             address = Integer.parseInt(subOperand);
-                        }catch (NumberFormatException e){
+                        } catch (NumberFormatException e) {
                             address = SymbolTable.getInstance().getSymbol(subOperand).getValue();
                         }
                     }
-                    binaryAddress = leftPad(convertDecToBin(address),20);
+                    binaryAddress = leftPad(convertDecToBin(address), 20);
                     break;
             }
 
@@ -157,19 +187,20 @@ public class ObjectCodeGenerator {
     }
 
     private void createOnjectCode() {
-        if (n != -1) {
-            String instructionObjectCode = convertBinaryToHex(instructionCode);
+        String instructionObjectCode = "";
+        if (n != -1 && n != -2) {
+            instructionObjectCode = convertBinaryToHex(instructionCode);
             if (instructionObjectCode.length() < 6)
                 instructionObjectCode = "0" + instructionObjectCode;
-            System.out.println("object code: " + instructionObjectCode);
-        }
+        } else if (n == -2)
+            instructionObjectCode = opcode;
+        System.out.println("object code: " + instructionObjectCode);
     }
 
-    private String convertBinaryToHex(String instructionCode) {
-        // convert to decimal first
-        int decimal = Integer.parseInt(instructionCode, 2);
-        // then convert to hex
-        return Integer.toString(decimal, 16);
+    private void printOutOfRangeError() {
+        System.out.println("DISPLACEMENT OUT OF RANGE, CANNOT USE PC OR BASE RELATIVE ADDRESSING");
+        p = 0;
+        b = 0;
     }
 
     private String setBPFlags(String str, Line line) {
@@ -178,33 +209,25 @@ public class ObjectCodeGenerator {
         int displacement = targetAddress - line.getAddress();
         System.out.println("disp: " + displacement);
         String binaryAddress = null;
-        if(displacement >= -2048 && displacement <= 2047)
-        {
-            p=1;
-            b=0;
+        if (displacement >= -2048 && displacement <= 2047) {
+            p = 1;
+            b = 0;
             binaryAddress = setBinaryAddress(displacement);
 
-        }
-        else if(line.isBaseRegisterSet())
-        {
+        } else if (line.isBaseRegisterSet()) {
             displacement = targetAddress - baseRegisterOperand;
             System.out.println("target address = " + targetAddress);
             System.out.println("base reg operand = " + baseRegisterOperand);
-            if(displacement >=0 && displacement < 4096)
-            {
-                b=1;
-                p=0;
+            if (displacement >= 0 && displacement < 4096) {
+                b = 1;
+                p = 0;
                 binaryAddress = setBinaryAddress(displacement);
 
                 System.out.println("binary address: " + binaryAddress);
-            }
-            else
-            {
+            } else {
                 printOutOfRangeError();
             }
-        }
-        else
-        {
+        } else {
             printOutOfRangeError();
         }
         if (binaryAddress.length() < 12)
@@ -212,10 +235,31 @@ public class ObjectCodeGenerator {
         return binaryAddress;
     }
 
-    private void printOutOfRangeError() {
-        System.out.println("DISPLACEMENT OUT OF RANGE, CANNOT USE PC OR BASE RELATIVE ADDRESSING");
-        p=0;
-        b=0;
+    private void setNIXFlags(String operandField) {
+        if (operandField.charAt(0) == '#') {
+            i = 1;
+            x = 0;
+            n = 0;
+            System.out.println("addressing mode: immediate");
+        } else if (operandField.charAt(0) == '@') {
+            n = 1;
+            i = 0;
+            x = 0;
+            System.out.println("addressing mode: indirect");
+        } else {
+            String trimmedOperand = operandField.trim();
+            if (trimmedOperand.charAt(trimmedOperand.length() - 2) == ',') {
+                n = 1;
+                i = 1;
+                x = 1;
+                System.out.println("addressing mode: direct, with indexing");
+            } else {
+                n = 1;
+                i = 1;
+                x = 0;
+                System.out.println("addressing mode: direct, without indexing");
+            }
+        }
     }
 
     private String setBinaryAddress(int displacement) {
@@ -243,68 +287,19 @@ public class ObjectCodeGenerator {
         return leftPad(binary, 8);
     }
 
-    private void setNIXFlags(String operandField) {
-        if (operandField.charAt(0) == '#') {
-            i = 1;
-            x = 0;
-            n = 0;
-            System.out.println("addressing mode: immediate" );
-        } else if(operandField.charAt(0) == '@'){
-            n = 1;
-            i = 0;
-            x = 0;
-            System.out.println("addressing mode: indirect");
-        } else {
-            String trimmedOperand = operandField.trim();
-            if (trimmedOperand.charAt(trimmedOperand.length() - 2) == ','){
-                n = 1;
-                i = 1;
-                x = 1;
-                System.out.println("addressing mode: direct, with indexing");
-            } else {
-                n = 1;
-                i = 1;
-                x = 0;
-                System.out.println("addressing mode: direct, without indexing");
-            }
-        }
+    private String convertBinaryToHex(String instructionCode) {
+        // convert to decimal first
+        int decimal = Integer.parseInt(instructionCode, 2);
+        // then convert to hex
+        return Integer.toString(decimal, 16);
     }
 
     private String convertDecToBin(int decimal) {
         return Integer.toBinaryString(decimal);
     }
 
-    private String convertDecToHex(int decimal){
+    private String convertDecToHex(int decimal) {
         return Integer.toHexString(decimal);
-    }
-
-    private static String leftPad(String str, int n){
-
-        String padString;
-
-        if(n==8)
-        {
-            padString = "00000000";
-            if(str.length() < 8 )
-                return padString.substring(str.length()) + str;
-        }
-        else if(n==4)
-        {
-            padString = "0000";
-
-            if(str.length() < 4)
-                return padString.substring(str.length()) + str;
-        }else if (n == 12) {
-
-            padString = "000000000000";
-
-            if(str.length() < 12)
-                return padString.substring(str.length()) + str;
-
-        }
-
-
-        return str;
     }
 
 }
